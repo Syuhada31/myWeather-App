86117273b85172eaef623468426be5ddb0fe77ab
Syu J <syuhada3137@gmail.com>
2021-10-17 20:19:31 +0800

    update API

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let now = new Date();
 
 let date = now.getDate();
 let hours = now.getHours();
-let minutes = now.getMinutes();
 if (hours < 10) {
   hours = `0${hours}`;
 }
-
+let minutes = now.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
