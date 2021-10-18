155b7ea06e01a6898e29dab72f7961ecbb506c13
Syu J <syuhada3137@gmail.com>
2021-10-18 23:42:25 +0800

    add relevant icon from API

--- script.js
+++ script.js
@@ -55,6 +55,14 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+
+document.querySelector("#icon").setAttribute(
+  "src",
+  'http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png'
+);
+
+document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
+
 }
 
 
