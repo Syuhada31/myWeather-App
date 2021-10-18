6b0721e07106a73662afcb5fe344a4e0d09430df
Syu J <syuhada3137@gmail.com>
2021-10-18 20:52:24 +0800

    update text decoration

--- script.js
+++ script.js
@@ -85,7 +85,6 @@ function heandleSubmit(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
-
   celsiusLink.classList.remove("active");
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
